7c9ee6cb4e2077e707c5f8714860532299f91bf6
bhandeystruck <adixb@hotmail.co.uk>
2021-03-23 23:39:07 -0400

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const articleRouter = require('./routes/articles')
 
+//express variable
 const app = express();
 
 //set up view engine to ejs
@@ -8,7 +9,7 @@ app.set('view engine', 'ejs')
 
 
 //tell the app to use the article router
-app.use('/articles',articleRouter)
+app.use('/articles', articleRouter)
 
 
 
@@ -17,8 +18,16 @@ app.use('/articles',articleRouter)
 
 //lets create a route using app, which takes in a request and a response
 app.get('/', (req,res)=>{
+    //creating an article variable to store the data for each article
+    const articles = [{
+        title:'Test Article',
+        createdAt: new Date(),
+        description: 'Test Description'
+    }]
+
     //to render the index.ejs
-    res.render('index')
+    //we can pass any object any keys
+    res.render('index', { articles: articles })
 })
 
 
